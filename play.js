893c5dacac061375072d0d3f20aeb56d45fc4533
andysongsong <sxj930916@gmail.com>
2021-07-28 21:23:13 +0000

    refactor client with es6 shortand syntax

--- play.js
+++ play.js
@@ -1,23 +1,2 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function (data) {
-  const conn = net.createConnection({
-    host: "135.23.223.133", // IP address here,
-    port: 50542, // PORT number here,
-  });
-  conn.on("connect", () => {
-    console.log("connected");
-  });
-  conn.on("data", (data) => {
-    console.log("new message", data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
 console.log("Connecting ...");
 connect();
