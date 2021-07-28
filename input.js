2cade51748708844ea7e667a0f21162477ce0818
andysongsong <sxj930916@gmail.com>
2021-07-28 23:47:40 +0000

    wasd movement

--- input.js
+++ input.js
@@ -1,16 +1,30 @@
-const setupInput = function () {
+let connection;
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", (key) => {
-    handleUserInput(key);
+    handleUserInput(key, connection);
     return stdin;
   });
 };
-const handleUserInput = function (key) {
+const handleUserInput = function (key, conn) {
   if (key === `\u0003`) {
     process.exit();
+  }
+  if (key === "w") {
+    conn.write("Move: up");
+  }
+  if (key === "s") {
+    conn.write("Move: down");
+  }
+  if (key === "a") {
+    conn.write("Move: left");
+  }
+  if (key === "d") {
+    conn.write("Move: right");
   }
 };
 
